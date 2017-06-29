df740628d7c54f21980221879d1a5b3da44a56d6
xrian <778811xxx@163.com>
2017-06-29 09:07:31 +0800

    将回调改为promise的方式

--- models/user.js
+++ models/user.js
@@ -6,8 +6,4 @@ let userSchema = require('../schema/user');
 
 let user = mongoose.model('User', userSchema);
 
-/*userSchema.methods.findOneByUsername = function (username, callback) {
-	return this.model('User').findOne({username: username}, callback);
-}*/
-
 module.exports = user;
--- routes/users.js
+++ routes/users.js
@@ -8,16 +8,24 @@ router.get('/', function (req, res, next) {
 	let returnJson = {
 		status: 0
 	};
-	UserModel.find({}, function (err, result) {
-		if (err) {
-			returnJson.msg = '查询失败,请刷新重试';
-			res.json(returnJson);
-		} else {
-			returnJson.status = 1;
-			returnJson.msg = 'success';
-			returnJson.data = result;
-			res.json(returnJson);
-		}
+	let promise = new Promise(function (resolve, reject) {
+		UserModel.find({}, function (err, result) {
+			if (err) {
+				reject(err);
+			} else {
+				resolve(result);
+			}
+		})
+	});
+	promise.then(function (result) {
+		returnJson.status = 1;
+		returnJson.msg = 'success';
+		returnJson.data = result;
+		res.json(returnJson);
+	}).catch(function (err) {
+		console.log(err);
+		returnJson.msg = '查询失败,请刷新重试';
+		res.json(returnJson);
 	})
 });
 
@@ -26,16 +34,24 @@ router.get('/:username', function (req, res, next) {
 		status: 0
 	}
 	let username = req.params.username;
-	UserModel.findOne({ 'username': username }, function (err, result) {
-		if (err) {
-			console.log('查询失败.', err);
-			res.json(returnJson);
-		} else {
-			returnJson.status = 1;
-			returnJson.msg = 'success';
-			returnJson.data = result;
-			res.json(returnJson);
-		}
+	let promise = new Promise(function (resolve, reject) {
+		UserModel.findOne({ 'username': username }, function (err, result) {
+			if (err) {
+				reject(err);
+			} else {
+				resolve(result);
+			}
+		});
+	})
+	promise.then(function (result) {
+		returnJson.status = 1;
+		returnJson.msg = 'success';
+		returnJson.data = result;
+		res.json(returnJson);
+	}).catch(function (err) {
+		console.log('查询失败.', err);
+		returnJson.msg('查询失败,请刷新重试');
+		res.json(returnJson);
 	});
 });
 
@@ -90,31 +106,42 @@ router.post('/', function (req, res, next) {
 	user.age = age;
 	user.sex = sex;
 	user.remarks = '用户注册';
-	UserModel.findOne({ 'username': username }, function (err, result) {
-		if (err) {
-			console.log('查询数据失败.', err);
-			returnJson.msg = '系统繁忙,请稍后再试';
-			res.json(returnJson);
-		} else {
-			if (result == null) {
-				UserModel.create(user, function (err, result) {
-					if (err) {
-						console.log(err);
-						returnJson.msg = '系统繁忙,请稍后再试';
-						res.json(returnJson);
-					} else {
-						console.log('保存成功.', result);
-						returnJson.status = 1;
-						returnJson.msg = '注册成功';
-						res.json(returnJson);
-					}
-				});
+
+	let findOnePromise = new Promise(function (resolve, reject) {
+		UserModel.findOne({ 'username': username }, function (err, result) {
+			if (err) {
+				console.log('查询数据失败.', err);
+				reject('系统繁忙,请稍后再试');
 			} else {
-				returnJson.msg = '该用户名已被注册,请更换用户名';
-				res.json(returnJson);
+				if (result == null) {
+					resolve(result);
+				} else {
+					reject('该用户名已被注册,请更换用户名');
+				}
 			}
-		}
+		})
 	})
+	findOnePromise.then(function (result) {
+		return new Promise(function (resolve, reject) {
+			UserModel.create(user, function (err, result) {
+				if (err) {
+					console.log(err);
+					reject('系统繁忙,请稍后再试');
+				} else {
+					resolve(result);
+				}
+			});
+		})
+	}).then(function (result) {
+		console.log('保存成功.', result);
+		returnJson.status = 1;
+		returnJson.msg = '注册成功';
+		res.json(returnJson);
+	}).catch(function (err) {
+		returnJson.msg = err;
+		res.json(returnJson);
+	})
+
 })
 
 router.put('/:username', function (req, res, next) {
@@ -161,37 +188,48 @@ router.put('/:username', function (req, res, next) {
 		res.json(returnJson);
 		return;
 	}
-	UserModel.findOne({ 'username': username }, function (err, result) {
-		if (err) {
-			console.log('查询报错', err);
-			returnJson.msg = '系统繁忙,请稍后再试';
-			res.json(returnJson);
-		} else {
-			if (result != null) {
-				result.nickname = nickname;
-				result.mobile = mobile;
-				result.age = age;
-				result.sex = sex;
-				result.remarks = '用户注册';
-				result.update_time = new Date();
-				result.save(function (err, result) {
-					if (err) {
-						console.log(err);
-						returnJson.msg = '系统繁忙,请稍后再试';
-						res.json(returnJson);
-					} else {
-						console.log('保存成功.', result);
-						returnJson.status = 1;
-						returnJson.msg = '修改成功';
-						res.json(returnJson);
-					}
-				});
+
+	let promise = new Promise(function (resolve, reject) {
+		UserModel.findOne({ 'username': username }, function (err, result) {
+			if (err) {
+				console.log('查询报错', err);
+				reject('系统繁忙,请稍后再试');
 			} else {
-				returnJson.msg = '该用户不存在,无法修改';
-				res.json(returnJson);
+				if (result != null) {
+					result.nickname = nickname;
+					result.mobile = mobile;
+					result.age = age;
+					result.sex = sex;
+					result.remarks = '用户注册';
+					result.update_time = new Date();
+					resolve(result);
+				} else {
+					reject('该用户不存在,无法修改');
+				}
 			}
-		}
+		})
 	})
+	promise.then(function (result) {
+		return new Promise(function (resolve, reject) {
+			result.save(function (err, results) {
+				if (err) {
+					console.log(err);
+					reject('系统繁忙,请稍后再试');
+				} else {
+					console.log('保存成功.', results);
+					resolve();
+				}
+			});
+		})
+	}).then(function () {
+		returnJson.status = 1;
+		returnJson.msg = '修改成功';
+		res.json(returnJson);
+	}).catch(function (err) {
+		returnJson.msg = err;
+		res.json(returnJson);
+	})
+
 
 })
 
@@ -200,29 +238,40 @@ router.delete('/:id', function (req, res, next) {
 		status: 0
 	};
 	let id = req.params.id;
-	UserModel.findById(id, function (err, result) {
-		if (err) {
-			console.log('查询报错', err);
-			returnJson.msg = '查询数据失败,请稍后再试';
-			res.json(returnJson);
-		} else {
-			if (result == null) {
-				returnJson.msg = '数据不存在,无法删除';
-				res.json(returnJson);
+	let promise = new Promise(function (resolve, reject) {
+		UserModel.findById(id, function (err, result) {
+			if (err) {
+				console.log('查询报错', err);
+				reject('查询数据失败,请稍后再试');
 			} else {
-				UserModel.remove({ _id: id }, function (err, result) {
-					if (err) {
-						console.log('删除报错:', err);
-						returnJson.msg = '删除失败,请刷新重试';
-					} else {
-						returnJson.status = 1;
-						returnJson.msg = 'success';
-					}
-					res.json(returnJson);
-				})
+				if (result == null) {
+					reject('数据不存在,无法删除');
+				} else {
+					resolve(id);
+				}
 			}
-		}
+		})
 	})
+	promise.then(function (id) {
+		return new Promise(function (resolve, reject) {
+			UserModel.remove({ _id: id }, function (err, result) {
+				if (err) {
+					console.log('删除报错:', err);
+					reject('删除失败,请刷新重试');
+				} else {
+					resolve();
+				}
+			})
+		})
+	}).then(function () {
+		returnJson.status = 1;
+		returnJson.msg = '保存成功';
+		res.json(returnJson);
+	}).catch(function (err) {
+		returnJson.msg = err;
+		res.json(returnJson);
+	})
+
 
 })
 
